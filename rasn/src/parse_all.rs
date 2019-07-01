00bd198f82d99b0ef450009536df38297c184466
jadamcrain <jadamcrain@gmail.com>
2019-07-01 13:37:55 -0700

    simplify the cert test now that we have the decoder

--- rasn/src/parse_all.rs
+++ rasn/src/parse_all.rs
@@ -43,74 +43,21 @@ mod tests {
     use parse_all::{parse_all, ParseHandler};
     use types::{ASNType, ASNError};
 
-    struct ParsePrinter {
-        indent: usize
+    struct MockHandler {}
+
+    impl ParseHandler for MockHandler {
+        fn begin_constructed(&mut self) -> () {}
+
+        fn end_constructed(&mut self) -> () {}
+
+        fn on_type(&mut self, _: &ASNType) -> () {}
+
+        fn on_error(&mut self, _: &ASNError) -> () {}
     }
-
-    impl ParsePrinter {
-        fn print_indent(&self) -> () {
-            for _ in 0..self.indent {
-                print!("  ");
-            }
-        }
-
-        fn new() -> ParsePrinter {
-            ParsePrinter { indent: 0 }
-        }
-    }
-
-    impl ParseHandler for ParsePrinter {
-        fn begin_constructed(&mut self) -> () {
-            self.indent += 1;
-        }
-
-        fn end_constructed(&mut self) -> () {
-            self.indent -= 1;
-        }
-
-        fn on_type(&mut self, asn: &ASNType) -> () {
-            self.print_indent();
-            println!("{}", asn);
-            match asn {
-                ASNType::BitString(cell) => {
-                    match cell.octets() {
-                        Some(octets) => {
-                            self.indent += 1;
-                            for chunk in octets.chunks(16) {
-                                self.print_indent();
-                                match chunk.split_last() {
-                                    Some((last, first)) => {
-                                        for byte in first {
-                                            print!("{:02X}:", byte)
-                                        }
-                                        println!("{:02X}", last)
-                                    }
-                                    None => {}
-                                }
-                            }
-                            self.indent -= 1;
-                        }
-                        None => ()
-                    }
-                }
-                _ => ()
-            }
-        }
-
-        fn on_error(&mut self, err: &ASNError) -> () {
-            println!("Error: {:?}", err);
-        }
-    }
-
 
     #[test]
-    fn iterates_over_x509() {
-
-        // TODO - figure out why there's an additional byte at the end of the DER cert
-        let cert = include_bytes!("../../x509/512b-rsa-example-cert.der");
-
-        parse_all(cert, &mut ParsePrinter::new()).unwrap()
+    fn parses_x509_without_error() {
+        // just checking that an error doesn't occur
+        parse_all(include_bytes!("../../x509/512b-rsa-example-cert.der"), &mut MockHandler {}).unwrap();
     }
-
-
 }
