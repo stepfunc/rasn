3b65f136bb5dc222b0921f6be8e52941e12c29ca
jadamcrain <jadamcrain@gmail.com>
2019-07-02 17:44:13 -0700

    now handling all non-optional fields

--- rasn/src/x509.rs
+++ rasn/src/x509.rs
@@ -31,8 +31,10 @@ pub enum AlgorithmParameters {
 pub struct TBSCertificate<'a> {
     pub serial_number : ASNInteger<'a>,
     pub signature : AlgorithmIdentifier,
-    pub issuer : &'a [u8], // punt for now and just put the struct bytes
-    pub validity: Validity
+    pub issuer : Name<'a>,
+    pub validity: Validity,
+    pub subject : Name<'a>,
+    pub subject_public_key_info : SubjectPublicKeyInfo<'a>
 }
 
 type Time = chrono::DateTime<chrono::FixedOffset>;
@@ -51,6 +53,46 @@ impl Validity {
         let mut parser = Parser::new(input);
         let value = Validity::new( parser.expect_utc_time()?,  parser.expect_utc_time()?);
         parser.expect_end()?;
+        Ok(value)
+    }
+}
+
+pub struct Name<'a> {
+    pub contents: &'a [u8]
+}
+
+impl<'a> Name<'a> {
+
+    fn new(contents: &'a [u8]) -> Name<'a> {
+        Name { contents}
+    }
+
+    fn parse(input: &[u8]) -> Result<Name, ASNError> {
+        Ok(Name::new(input))
+    }
+}
+
+pub struct SubjectPublicKeyInfo<'a> {
+    pub algorithm: AlgorithmIdentifier,
+    pub subject_public_key: ASNBitString<'a>
+}
+
+impl<'a> SubjectPublicKeyInfo<'a> {
+
+    fn new(algorithm: AlgorithmIdentifier, subject_public_key: ASNBitString<'a>) -> SubjectPublicKeyInfo<'a> {
+        SubjectPublicKeyInfo { algorithm, subject_public_key}
+    }
+
+    fn parse(input: &[u8]) -> Result<SubjectPublicKeyInfo, ASNError> {
+        let mut parser = Parser::new(input);
+
+        let value = SubjectPublicKeyInfo::new(
+            AlgorithmIdentifier::parse(parser.expect_sequence()?)?,
+            parser.expect_bit_string()?
+        );
+
+        parser.expect_end()?;
+
         Ok(value)
     }
 }
@@ -104,29 +146,35 @@ impl<'a> TBSCertificate<'a> {
 
     pub fn new(serial_number : ASNInteger<'a>,
                signature : AlgorithmIdentifier,
-               issuer : &'a [u8],
-               validity: Validity) -> TBSCertificate<'a> {
-        TBSCertificate { serial_number, signature, issuer, validity }
+               issuer : Name<'a>,
+               validity: Validity,
+               subject : Name<'a>,
+               subject_public_key_info : SubjectPublicKeyInfo<'a>) -> TBSCertificate<'a> {
+        TBSCertificate { serial_number, signature, issuer, validity, subject, subject_public_key_info }
     }
 
     fn parse(input: &[u8]) -> Result<Constructed<TBSCertificate>, ASNError> {
 
         let mut parser = Parser::new(input);
 
-        let tbs_certificate = Constructed::new(
+        let value = Constructed::new(
             input,
             TBSCertificate::new(
                 parser.expect_integer()?,
                 AlgorithmIdentifier::parse(parser.expect_sequence()?)?,
-                parser.expect_sequence()?,
-                Validity::parse(parser.expect_sequence()?)?
+                Name::parse(parser.expect_sequence()?)?,
+                Validity::parse(parser.expect_sequence()?)?,
+                Name::parse(parser.expect_sequence()?)?,
+                SubjectPublicKeyInfo::parse(parser.expect_sequence()?)?
             )
         );
 
-        // TODO
-        // parser.expect_end()?;
+        // TODO - handle optional fields!
 
-        Ok(tbs_certificate)
+        parser.expect_end()?;
+
+
+        Ok(value)
     }
 }
 
